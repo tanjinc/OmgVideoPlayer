02530afd2afb1c3c0976e06351ebc514291e8073
谭金城 <tanjincheng@meizu.com>
2018-08-15 10:34:46 +0800

    fix bug

--- app/src/main/java/com/tanjinc/omgvideoplayer/MainActivity.java
+++ app/src/main/java/com/tanjinc/omgvideoplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.tanjinc.omgvideoplayer.detailPage.DetailActivity;
 import com.tanjinc.omgvideoplayer.floatPage.FloatDemoActivity;
 import com.tanjinc.omgvideoplayer.listPage.ListVideoActivity;
 
@@ -17,7 +18,7 @@ import com.tanjinc.playermanager.R;
  * Created by tanjinc on 17-4-5.
  */
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private static final String TAG = "TestActivity";
 
 
@@ -26,22 +27,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.test_main_layout);
 
-        findViewById(R.id.test_video_list_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),ListVideoActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        findViewById(R.id.test_video_float_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),FloatDemoActivity.class);
-                startActivity(intent);
-            }
-        });
-
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.test_video_list_btn: {
+                Intent intent = new Intent(getApplicationContext(),ListVideoActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.test_video_detail_btn: {
+                Intent intent = new Intent(getApplicationContext(),DetailActivity.class);
+                startActivity(intent);
+                break;
+            }
+            case R.id.test_video_float_btn: {
+                Intent intent = new Intent(getApplicationContext(),FloatDemoActivity.class);
+                startActivity(intent);
+                break;
+            }
+        }
+    }
 }
